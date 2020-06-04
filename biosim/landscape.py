43a40bebcf9b0f3c76f9301e52c5a0ba0366948c
Ashesh Raj Gnawali <asgn@nmbu.no>
2020-06-04 16:23:41 +0200

    created ladscape.py and added methods

---
+++ biosim/landscape.py
@@ -0,0 +1,31 @@
+__author__ = "Ashesh Raj Gnawali, Maritn Bø"
+__email__ = "asgn@nmbu.no & mabo@nmbu.no"
+
+
+class Landscape:
+    """
+    Parent class for various landscapes water, desert, highland and lowland
+    """
+
+    def __init__(self):
+        pass
+
+
+class Water(Landscape):
+    def __init__(self):
+        pass
+
+
+class Desert(Landscape):
+    def __init__(self):
+        pass
+
+
+class Highland(Landscape):
+    def __init__(self):
+        pass
+
+
+class Lowland(Landscape):
+    def __init__(self):
+        pass
