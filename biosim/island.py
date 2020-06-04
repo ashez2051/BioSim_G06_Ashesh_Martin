2d416ef8f66a821fb08dae9271262613eefd0db7
Ashesh Raj Gnawali <asgn@nmbu.no>
2020-06-04 15:35:54 +0200

    created island.py

---
+++ biosim/island.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Island class which gives a geographical structure of the multiline string provided.
+Returns a numpy array with cell information equal to the number of characters in given string
+"""
+__author__ = "Ashesh Raj Gnawali, Maritn Bø"
+__email__ = "asgn@nmbu.no & mabo@nmbu.no"
+
+class Island:
+    """
+    This class represents the given map string as an array of objects
+    """
+    def __init__(self, ):
+        pass
+
+
