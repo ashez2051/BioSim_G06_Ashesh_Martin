ee71871b38a13098ce3e8f4666f6a6eba4f75f22
Ashesh Raj Gnawali <asgn@nmbu.no>
2020-06-04 10:44:29 +0200

    added methods that are common to both herbivores and carnivores

--- biosim/fauna.py
+++ biosim/fauna.py
@@ -2,11 +2,11 @@
 
 """
 """
-__author__ = "Ashesh  Maritn Bø"
+__author__ = "Ashesh Raj Gnawali, Maritn Bø"
 __email__ = "asgn@nmbu.no & mabo@nmbu.no"
 
-
 import numpy as np
+import math
 
 
 class Fauna:
@@ -16,7 +16,7 @@ class Fauna:
 
     parameters = {}
 
-    def __init__(self, age = None, weight = None):
+    def __init__(self, age=None, weight=None):
         """
         Constructor for the parent class fauna with age and weight of the animals
         :param age: Age of the animal, integer
@@ -27,31 +27,63 @@ class Fauna:
         else:
             self.age = age
         if weight is None:
-            self.weight = np.random.normal(self.parameters["w_birth"], self.parameters["sigma_birth"])
+            self.weight = np.random.normal(self.parameters["w_birth"],
+                                           self.parameters["sigma_birth"])
         else:
             self.weight = weight
-        #Define "things" that applies for both animals
+        # Define "things" that applies for both animals
         self.fitness = 0
         self.gives_birth = False
 
+    @property
+    def animal_weight(self):
+        """
+        :return: weight of the animal
+        """
+        return self.weight
 
+    def animal_weight_with_age(self):
+        """
+        Updates the age of an animal and it's weight at the end of the year
+        """
+        self.weight = self.parameters['eta'] * self.weight
+        self.age += 1
 
+    def animal_weight_with_food(self, food_eaten):
+        """
+        Updates the weight of an animal based on it's feeding behavior
+        :param food_eaten: the amount of food eaten by an animal, float
+        """
+        self.weight += self.parameters['beta'] * food_eaten
 
-    def many_functions(self): #Give birth etc
-        #Add this later, start on these after 14.30
+    @property
+    def animal_fitness(self):
+        """"
+        Calculates the fitness of an animal based on age and weight
+        """
+        if self.weight > 0:
+            q_pos = 1/(1+np.exp(self.parameters['phi_age'] *
+                                (self.age - self.parameters['a_half'])))
+
+            q_neg = 1/(1+ np.exp(self.parameters['phi_weight'] *
+                                 (self.weight - self.parameters['w_half'])))
+
+            return q_neg * q_pos
+        else:
+            return 0
+        
+
 
 
 class Herbivore(Fauna):
     """
     Child class of Fauna
     """
-    parameters = {"w_birth": 8.0, "sigma_birth": 1.5,"beta": 0.9,
-                  "eta": 0.05, "a_half": 40.0, "phi_age": 0.6,
-                  "w_half": 10.0, "phi_weight": 0.1, "mu": 0.25,
-                  "lambda": 1.0, "gamma": 0.2, "zeta": 3.5,
-                  "xi": 1.2, "omega": 0.4, "F": 10.0 }
+    parameters = {"w_birth": 8.0, "sigma_birth": 1.5, "beta": 0.9, "eta": 0.05, "a_half": 40.0,
+                  "phi_age": 0.6, "w_half": 10.0, "phi_weight": 0.1, "mu": 0.25, "lambda": 1.0,
+                  "gamma": 0.2, "zeta": 3.5, "xi": 1.2, "omega": 0.4, "F": 10.0}
 
-    def __init__(self, age = None, weight = None):
+    def __init__(self, age=None, weight=None):
         super().__init__(age, weight)
         self.parameters = Herbivore.parameters
 
@@ -62,12 +94,6 @@ class Carnivore(Fauna):
     """
     parameters = {}
 
-    def __init__(self, age = None, weight = None):
+    def __init__(self, age=None, weight=None):
         super().__init__(age, weight)
         self.parameters = Carnivore.parameters
-
-
-
-
-
-
