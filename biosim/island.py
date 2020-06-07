44805581b2d52748999fffaa0c71a096c9e57e49
Ashesh Raj Gnawali <asgn@nmbu.no>
2020-06-07 13:37:14 +0200

    Wrote code for simulation of herbivores in one place

--- biosim/island.py
+++ biosim/island.py
@@ -7,7 +7,7 @@ Returns a numpy array with cell information equal to the number of characters in
 __author__ = "Ashesh Raj Gnawali, Maritn Bø"
 __email__ = "asgn@nmbu.no & mabo@nmbu.no"
 
-from biosim.landscape import * #ask about how to import this
+from biosim.landscape import *  # ask about how to import this
 import numpy as np
 import textwrap
 
@@ -23,8 +23,7 @@ class Island:
         self.check_edge_cells_is_water(self.map)
 
         self.landscape_dict = {'W': Water, 'D': Desert, 'L': Lowland, 'H': Highland}
-        self.fauna_dict = {'Herbivore': Herbivore,
-                           # 'Carnivore': Carnivore
+        self.fauna_dict = {'Herbivore': Herbivore, # 'Carnivore': Carnivore
                            }
 
         self._cells = self.array_with_landscape_objects()
@@ -56,8 +55,11 @@ class Island:
         :return: the map edges
         """
         rows, cols = island_array.shape[0], island_array[1]
-        island_edges = island_array[0, :cols], island_array[rows - 1, :cols],\
-                       island_array[:rows - 1, 0], island_array[:rows - 1, cols - 1]
+        island_edges = island_array[0, :cols], island_array[rows - 1, :cols], island_array[
+                                                                              :rows - 1,
+                                                                              0], island_array[
+                                                                                  :rows - 1,
+                                                                                  cols - 1]
         return island_edges
 
     def check_edge_cells_is_water(self, island_array):
@@ -77,13 +79,12 @@ class Island:
         objects corresponding to the landscape letter instead of a string
         :return: an array with the landscape objects
         """
-        landscape_cell_object = np.empty(self.map.shape, dtype = object)
+        landscape_cell_object = np.empty(self.map.shape, dtype=object)
         for row in np.arange(self.map.shape[0]):
             for col in np.arange(self.map.shape[1]):
                 landscape_type = self.map[row][col]
                 landscape_cell_object[row][col] = self.landscape_dict[landscape_type]()
         return landscape_cell_object
-
 
     def add_animals(self, pop):
         """
@@ -103,27 +104,32 @@ class Island:
                 cell.add_animal(animal_object)
 
 
-
 if __name__ == "__main__":
     geogr = """\ 
     WWW
     WLW
     WWW"""
     geogr = textwrap.dedent(geogr)
-    ini_herbs = [{"loc": (1, 2), "pop": [
-        {"species": "Herbivore", "age": 5, "weight": 20} for _ in range(50)]}]
+    ini_herbs = [{"loc": (1, 2),
+                  "pop": [{"species": "Herbivore", "age": 5, "weight": 20} for _ in range(50)]}]
 
-    self.landscapes = {'W': Water, 'D': Desert, 'L': Lowland, 'H': Highland}
-    self.landscapes_with_parameters = [Lowland]
+    animal_species = {'Carnivore': Carnivore, 'Herbivore': Herbivore}
 
-    self.animal_species = {'Carnivore': Carnivore, 'Herbivore': Herbivore}
-
-    lengths = [len(line) for line in island_map.splitlines()]
+    lengths = [len(line) for line in Island_map.splitlines()]
     if len(set(lengths)) > 1:
         raise ValueError('This given string is not uniform')
-    self.island_map = island_map
-    self._map = Island(island_map)
-    random.seed(seed)
-    self.add_population(ini_pop)
 
+    landscape_dict = Island.landscape_dict
 
+    for _ in range(100):
+        print("New Year")
+
+        rows, cols = Island.map_dims
+
+        for row in range(rows):
+            for col in range(cols):
+                Island.cells[row, col].animal_eats()
+                Island.cells[row, col].animal_gives_birth()
+                Island.cell[row, col].add_children_to_adult_animals()
+                Island.cell[row, col].update_animal_weight_and_age()
+                Island.cell[row, col].animal_dies()
