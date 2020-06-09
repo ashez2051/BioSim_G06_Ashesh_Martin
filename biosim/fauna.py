8c0bcdbb990b58b590ad529a0adbae4a9b6d1e09
Ashesh Raj Gnawali <asgn@nmbu.no>
2020-06-09 09:58:54 +0200

    added migration parameter lambda

--- biosim/fauna.py
+++ biosim/fauna.py
@@ -134,7 +134,7 @@ class Herbivore(Fauna):
     """
     parameters = {"w_birth": 8.0, "sigma_birth": 1.5, "beta": 0.9, "eta": 0.05, "a_half": 40.0,
                   "phi_age": 0.6, "w_half": 10.0, "phi_weight": 0.1, "mu": 0.25, "gamma": 0.2,
-                  "zeta": 3.5, "xi": 1.2, "omega": 0.4, "F": 10.0}
+                  "lambda": 1, "zeta": 3.5, "xi": 1.2, "omega": 0.4, "F": 10.0}
 
     def __init__(self, age=None, weight=None):
         super().__init__(age,
@@ -147,7 +147,8 @@ class Carnivore(Fauna):
     """
     parameters = {"w_birth": 6.0, "sigma_birth": 1, "beta": 0.75, "eta": 0.125, "a_half": 40.0,
                   "phi_age": 0.3, "w_half": 4.0, "phi_weight": 0.4, "mu": 0.4, "gamma": 0.8,
-                  "zeta": 3.5, "xi": 1.1, "omega": 0.8, "F": 50.0, "delta_phi_max": 10.0}
+                  "lambda": 1, "zeta": 3.5, "xi": 1.1, "omega": 0.8, "F": 50.0,
+                  "delta_phi_max": 10.0}
 
     def __init__(self, age=None, weight=None):
         super().__init__(age, weight)  # self.parameters = Carnivore.parameters
