08a5ecfc21dbc838c725c384d563a77de2913ddd
Martin Boe <martin.bo@live.no>
2020-06-14 14:57:26 +0200

    Commented out param changes
    Used default ones in simulation

--- biosim/simulation.py
+++ biosim/simulation.py
@@ -70,7 +70,7 @@ class BioSim:
         self.add_population(ini_pop)
 
         if ymax_animals is None:
-            self.ymax_animals = 20000
+            self.ymax_animals = 30000
         else:
             self.ymax_animals = ymax_animals
 
